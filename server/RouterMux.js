d57946495a64c8d11f2aab7f5b863b506012e378
mohanraj v <mohanrajvengi@gmail.com>
2020-11-19 13:40:21 +0530

    Model_2

--- server/RouterMux.js
+++ server/RouterMux.js
@@ -7,6 +7,7 @@ const Router = Express.Router();
 // imports
 const DataRouter = require("../router/DataRouter.js");
 Router.use(DataRouter);
+
 // Testing Router
 Router.get("/ping", (req, res) => {
     res.send("pong from server");
--- server/Server.js
+++ server/Server.js
@@ -2,7 +2,6 @@
 const Express = require('express');
 const Env = require("dotenv").config({ path: require("find-config")(".env") });
 
-
 // Imports
 const Server = Express();
 const CONTEXT_PATH = Env.parsed.CONTEXT_PATH;
@@ -11,29 +10,22 @@ const RouterMux = require('./RouterMux.js');
 const Database = require('../database/Database.js');
 const cors=require('cors');
 
-
 // App Service Start Indication
 console.log(new Date().toString() + " App service started");
 
-
 // Database Initialization
 Database.InitilizeDatabase();
-
 
 // Parsing User data
 Server.use(Express.json());
 Server.use(Express.urlencoded({ extended: true }));
 Server.use(cors());
 
-
 // Router
 Server.use(CONTEXT_PATH, RouterMux);
-
 
 // Server is listening to port
 Server.listen(PORT, () => {
 	console.log(new Date().toString() + ' Server is listening to port ' + Env.parsed.PORT);
 });
-
-
 module.exports = { Server };
